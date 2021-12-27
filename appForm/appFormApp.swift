c23e8fbb3d644d84fac53d30616fce6c84e6fc69
lorena <lorenaggama@gmail.com>
2021-12-27 16:51:20 -0300

    Initial Commit

---
+++ appForm/appFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  appFormApp.swift
+//  appForm
+//
+//  Created by Lorena Gama on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct appFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
